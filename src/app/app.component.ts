c7baf043316a9f89a17472085acd4a36edef31c8
Your Name <you@example.com>
2022-09-06 20:02:14 +0200

    Add detail display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FoodApp';
-  loadedFeature = "recipe"
+  loadedFeature = "recipe";
+  
   onNavigate(feature:string) {
     this.loadedFeature = feature;
   }
