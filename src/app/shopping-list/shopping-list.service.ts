eb272c9123369b18b8becf2ef0a4116b29d93dd9
Your Name <you@example.com>
2022-09-08 16:52:01 +0200

    Adding Shopping List\

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -15,12 +15,17 @@ export class ShoppingListService {
   constructor() { }
 
   getIngredients(){
-    return this.ingredients.slice()
+    return this.ingredients.slice();
 
   }
   addIngredient(ingredient : Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.emit(this.ingredients.slice());
+
+  }
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice())
 
   }
 
