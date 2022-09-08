16a09e8c1a2b29d8ca1c85e4b2c9cd85e02cdf52
Your Name <you@example.com>
2022-09-08 16:53:33 +0200

    Working on Shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 
@@ -8,13 +10,28 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe("Cheese", "Test recipe.", "https://upload.wikimedia.org/wikipedia/commons/6/61/Trappista_cheese_original.jpg"),
-    new Recipe("More Cheese", "Another test recipe.", "https://upload.wikimedia.org/wikipedia/commons/d/dd/Wikicheese_-_Brie_de_Melun_-_20150515_-_015.jpg")
+    new Recipe("Cheese", "Test recipe.", "https://upload.wikimedia.org/wikipedia/commons/6/61/Trappista_cheese_original.jpg", [
+      new Ingredient('Ser', 1),
+      new Ingredient("super ser", 2)
+    ]),
+
+
+    new Recipe("More Cheese", "Another test recipe.", "https://upload.wikimedia.org/wikipedia/commons/d/dd/Wikicheese_-_Brie_de_Melun_-_20150515_-_015.jpg",
+    [
+      new Ingredient("Serowy Ser",1),
+      new Ingredient("Bardziej Srerowy Ser", 2)
+    ])
   ];
+  constructor(private slService : ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
   }
-
-  constructor() { }
+  
+  
+  addIngredientsToShoppingList(ingredients:Ingredient[] ){
+    this.slService.addIngredients(ingredients);
+    
+  }
+  
 }
