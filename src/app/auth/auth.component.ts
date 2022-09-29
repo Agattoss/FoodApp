646d345a697758e1b00785d28bf9115d67a2baca
Agata <108717863+Agattoss@users.noreply.github.com>
2022-09-29 13:37:03 +0200

    Add AuthGuard

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -12,30 +14,40 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
     const email = form.value.email;
     const password = form.value.password;
-    this.isLoading = true;
-    if(this.isLoginMode){
 
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
+    if(this.isLoginMode){
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-       {next: responseData => {
-          console.log(responseData);
-          this.isLoading = false;
-        },
-        error: errorMessage => {
-          console.log(errorMessage);
-          this.error= errorMessage.message;
-          this.isLoading = false;
-        }}
-      );
+      authObs= this.authService.signup(email, password);
     }
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+        this.router.navigate(['/recipes'])
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
     form.reset();
   }
 
