bbde5ee1016b83d4c10cb563a9f3d45b82f9a7ef
Agata <108717863+Agattoss@users.noreply.github.com>
2022-10-15 15:54:01 +0200

    deleting placeholder directive

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,11 @@
-import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
 import { Store } from '@ngrx/store';
-import { AlertComponent } from '../shared/alert/alert.component';
-import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+
+
 
 
 @Component({
@@ -17,14 +17,14 @@ export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
-  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+
 
 
 private closeSub: Subscription;
 private storeSub: Subscription;
 
   constructor(
-    private componentFactoryResolver: ComponentFactoryResolver,//////to change
+
     private store: Store<fromApp.AppState>) {}
 
 
@@ -59,9 +59,7 @@ private storeSub: Subscription;
     this.storeSub = this.store.select('auth').subscribe(authState => {
       this.isLoading = authState.loading;
       this.error = authState.authError;
-      if (this.error) {
-        this.showErrorAlert(this.error);
-      }
+
     });
   }
 
@@ -73,19 +71,5 @@ private storeSub: Subscription;
       this.storeSub.unsubscribe();
     }
   }
-  private showErrorAlert(message: string) {
-    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
-      AlertComponent
-    );
-    const hostViewContainerRef = this.alertHost.viewContainerRef;
-    hostViewContainerRef.clear();
 
-    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
-
-    componentRef.instance.message = message;
-    this.closeSub = componentRef.instance.close.subscribe(() => {
-      this.closeSub.unsubscribe();
-      hostViewContainerRef.clear();
-    });
-  }
 }
