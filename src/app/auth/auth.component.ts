eb11deacadf2984256576b9ace773b1239dc9e4d
Agata <108717863+Agattoss@users.noreply.github.com>
2022-10-15 16:07:10 +0200

    clean up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import * as fromApp from '../store/app.reducer';
